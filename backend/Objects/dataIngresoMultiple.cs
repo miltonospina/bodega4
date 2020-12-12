10a4e01a3fd14b2b3c797c5fc123369a16e94eab
miltonospina <miltonospina@gmail.com>
2020-12-11 20:58:59 -0500

    Backend ingreso multiple funcionando

---
+++ backend/Objects/dataIngresoMultiple.cs
@@ -0,0 +1,9 @@
+using b4backend.Models;
+
+namespace b4backend.Objects
+{
+public class dataIngresoMultiple{
+        public int cantidad {get; set;}
+        public Movimientos ingreso {get; set;}
+    }
+}
